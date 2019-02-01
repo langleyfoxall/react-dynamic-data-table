4c23a7d17f420ca2ca2c769b59e974341dbd80be
Dexter Marks-Barber <dexter.mb@outlook.com>
2019-02-01 08:58:31 +0000

    Restructure imports to be more friendly

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import DynamicDataTable from './DynamicDataTable';
+import AjaxDynamicDataTable from './AjaxDynamicDataTable';
+
+import DataRow from './Components/DataRow';
+import Pagination from './Components/Pagination';
+
+import withFilter from './Wrappers/withFilter';
+
+export {
+    DynamicDataTable,
+    AjaxDynamicDataTable,
+    DataRow,
+    Pagination,
+    withFilter
+};
+
+export default DynamicDataTable;
